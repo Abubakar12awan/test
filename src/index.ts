03e12e5bef9813122c05cd28ce9a2811250eb21e
Abu bakar <106442407+Abubakar12awan@users.noreply.github.com>
2024-10-15 18:58:39 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,6 @@ import {authController, controllers, initializeControllers} from "./domain/contr
 import {verifyJwtAccessToken} from "./utils/jwt-utils";
 import {AsklessServer} from "askless";
 
-
 AppDataSource.initialize().then(async () => {
     const server = new AsklessServer<number>();
 
@@ -14,19 +13,22 @@ AppDataSource.initialize().then(async () => {
         controller.initializeRoutes(server);
     }
 
+    // Use the dynamic port provided by Heroku, or default to 3000 for local development
+    const PORT = process.env.PORT ? parseInt(process.env.PORT) : 3000;
+
     server.init({
-        wsOptions: { port: 3000, },
+        wsOptions: { port: PORT },  // Replace hardcoded port with the dynamic one
         debugLogs: false,
         sendInternalErrorsToClient: false,
         requestTimeoutInMs: 7 * 1000,
-        authenticate: async (credential, accept, reject) : Promise<void> => {
+        authenticate: async (credential, accept, reject): Promise<void> => {
             if (credential && credential["accessToken"]) {
                 const result = verifyJwtAccessToken(credential["accessToken"]);
                 if (!result.valid) {
                     reject({credentialErrorCode: "EXPIRED_ACCESS_TOKEN"});
                     return;
                 }
-                accept.asAuthenticatedUser({ userId: result.userId,  });
+                accept.asAuthenticatedUser({ userId: result.userId });
                 return;
             }
 
@@ -35,6 +37,6 @@ AppDataSource.initialize().then(async () => {
     });
 
     server.start();
-    console.log("started on "+server.localUrl);
+    console.log("Server started on " + server.localUrl + " and running on port " + PORT);
 
-}).catch(databaseError => console.log(databaseError))
+}).catch(databaseError => console.log(databaseError));
