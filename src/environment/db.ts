0ef6046efba5a93faa8efe34672b652be71fb1c7
Abu bakar <106442407+Abubakar12awan@users.noreply.github.com>
2024-10-15 18:15:02 +0500

    Update db.ts

--- src/environment/db.ts
+++ src/environment/db.ts
@@ -3,11 +3,11 @@ import {TextMessageEntity} from "../domain/entity/text-message-entity";
 import {UserEntity} from "../domain/entity/user-entity";
 export const dbDatasourceOptions: DataSourceOptions = {
     type: "mysql",
-    host: "127.0.0.1", // Host should not include the port
+    host: "qzkp8ry756433yd4.cbetxkdyhwsb.us-east-1.rds.amazonaws.com", // Host should not include the port
     port: 3306,        // Port is already specified separately
-    username: "root",
-    password: "root",
-    database: "malik",
+    username: "kchqblfc94b4ki51",
+    password: "slcyd7ymqlbh3nap",
+    database: "h4j3sy4y3xck3nnq",
 
     synchronize: true,
     logging: false,
